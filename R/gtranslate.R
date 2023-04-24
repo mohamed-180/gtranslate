ea8adb7f768b62fb10cef91bcbb2a765441287cf
mohamedd180 <mohamed_d180@hotmail.com>
2023-04-24 02:49:58 +0200

    import httr

--- R/gtranslate.R
+++ R/gtranslate.R
@@ -11,13 +11,14 @@
 #' @examples
 translate <- function(text, from = "auto", to = "ar", trim_str = TRUE){
 
+
   stopifnot(is.character(text))
 
   text = trimws(text)
 
   url <- "https://translate.google.com/m"
 
-  sapply(text ,
+  sapply(text,
          function(x){
 
            if(x == "") return("")
@@ -26,9 +27,8 @@ translate <- function(text, from = "auto", to = "ar", trim_str = TRUE){
              if(trim_str) x = substr(x, 1, 4999)
              else stop("The length of the text must be less than 5000 characters.")
            }
-
            r <- httr::GET(url, query = list(sl = from, tl = to, q = x))
-           p <- rvest::read_html(content(r, 'text'))
+           p <- rvest::read_html(httr::content(r, 'text'))
            rvest::html_text2(rvest::html_elements(p, xpath = '//div[@class="result-container"]'))
          },
          USE.NAMES = FALSE)
